2d7f4c781189e53de57c534228fa08bfde433503
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-04-01 15:01:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,64 @@
-import React from 'react'
+import React from 'react';
 import '../styles/App.css';
+
 const App = () => {
+  const handleInput = (event) => {
+    const inputType = event.target.type;
+    const inputId = event.target.id;
+    const inputValue = event.target.value;
 
-  const handleInput = (event) =>{
-   // use console.log
-  }
+    if (inputType === "text") {
+      console.log(`Input in #${inputId} is ${inputValue}`);
+    } else if (inputType === "number") {
+      const otherInputValue = document.getElementById("text-input").value;
+      console.log(`Input in #${inputId} is ${otherInputValue}${inputValue}`);
+    }
+  };
 
-  // do not change id of input elements
   return (
     <div id="main">
-      <label htmlFor='text-input'>Text Input:- </label>
-      <input id="text-input" type={'text'} />
+      <label htmlFor="text-input">Text Input:- </label>
+      <input id="text-input" type="text" onChange={handleInput} />
 
-      <br/>
-      <br/>
+      <br />
+      <br />
 
-      <label htmlFor='num-input'>Number input</label>
-      <input id="num-input"  type={'number'} />
-      <br/>
+      <label htmlFor="num-input">Number input</label>
+      <input id="num-input" type="number" onChange={handleInput} />
+      <br />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
+
+
+
+
+
+// import React from 'react'
+// import '../styles/App.css';
+// const App = () => {
+
+//   const handleInput = (event) =>{
+//    // use console.log
+//   }
+
+//   // do not change id of input elements
+//   return (
+//     <div id="main">
+//       <label htmlFor='text-input'>Text Input:- </label>
+//       <input id="text-input" type={'text'} />
+
+//       <br/>
+//       <br/>
+
+//       <label htmlFor='num-input'>Number input</label>
+//       <input id="num-input"  type={'number'} />
+//       <br/>
+//     </div>
+//   )
+// }
+
+
+// export default App;
